670d188821e83c267caeb44bfa2c20e1b2e215a8
bascouba <bastien.bascou1@gmail.com>
2017-06-29 17:54:27 +0200

    add image

--- ses_imagotag/__manifest__.py
+++ ses_imagotag/__manifest__.py
@@ -9,21 +9,18 @@
         Manage your labels. Link your products to labels that will display informations about the products
     """,
 
+    'images': [],
     'author': "Bastien BASCOU, SES-imagotag",
     'installable': True,
     'application': True,
     'website': "www.ses-imagotag.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Inventory, Point of Sale',
+
+    'category': 'Marketing, Inventory, Point of Sale',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['point_of_sale'],
 
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/label.xml',
